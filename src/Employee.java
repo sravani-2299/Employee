ede2ca64a70c281073eb03eba7386a5e3a27ccbe
kshas <kshas@LAPTOP-O8IPP6MH>
2023-10-16 11:33:42 +0530

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee {
+		int id=1;
+		String name="shashi";
+		String company="Amazon";
+		 public static void main(String[] args) {
+			Employee e1=new Employee();
+			Employee e2=new Employee();
+			System.out.println(e1);
+		}
+
+	
+
+}
